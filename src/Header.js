8a5eda03ac80208334f6323e632a3907c7492509
Scott <scottgangemi@gmail.com>
2017-12-13 07:22:32 +1100

    work on header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import glamorous from 'glamorous';
+import LogoSG from './logo.png';
 
 const HeaderContainer = glamorous.div({
   backgroundColor: '#22BBB5',
@@ -22,30 +23,50 @@ const SelfPromotionHeader = glamorous.div({
   padding: '50px',
   display: 'flex',
   justifyContent: 'space-around',
+  '@media(max-width: 480px)': {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
+
+const InnerSelfPromotionHeader = glamorous.div({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
 });
 
 const A = glamorous.a({
   cursor: 'pointer',
+  color: 'white',
+  textDecoration: 'none',
   ':hover': {
-    borderBottom: '2px white thick',
+    textDecoration: 'underline',
   },
+});
 
+const Logo = glamorous.div({
+  background: `url(${LogoSG})`,
+  height: '150px',
+  width: '150px',
+  backgroundPositionX: 'center',
+  backgroundPositionY: 'center',
+  backgroundSize: '150px',
 });
 
 const Header = () => (
   <HeaderContainer>
     <LinkContainer>
-      <A>Github</A>
-      <A>LinkedIn</A>
+      <A href="https://www.github.com/Gangezilla">Github</A>
+      <A href="https://www.linkedin.com/in/scott-gangemi-18146198/">LinkedIn</A>
       <span>0431 987 232</span>
       <span>s.gangemi@me.com</span>
     </LinkContainer>
     <SelfPromotionHeader>
-      <div>
+      <InnerSelfPromotionHeader>
         <h1>Scott Gangemi</h1>
         <p>Title for yourself goes here.</p>
-      </div>
-      <span>Logo</span>
+      </InnerSelfPromotionHeader>
+      <Logo />
     </SelfPromotionHeader>
   </HeaderContainer>
 );
