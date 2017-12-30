23fbfbdb7b2c9f2018732bb4b391f01465223406
Scott <scottgangemi@gmail.com>
2017-12-30 12:15:19 +1100

    update personal summary

--- src/Components/PersonalSummary/index.js
+++ src/Components/PersonalSummary/index.js
@@ -12,15 +12,13 @@ const PersonalSummary = () => (
       <HeyThere> Hey there, </HeyThere>
       <P>
         I&apos;m Scott. I really like dogs, and I think this is the best photo of me that&apos;s ever going to be taken.
-        I’m a front end developer + designer and I’m passionate about solving cool problems, teaching,
-        and seeing the impact of awesome work.
+        I&apos;m a front end (and aspiring full stack) developer whose currently working at Domain Group in Sydney, Australia. I&apos;m very interested in solving cool problems, teaching, and seeing the impact of awesome work. I do most of my work in React and Node; I really just love JS.
       </P>
       <P>
-        I&apos;m interested in designing and creating fantastic experiences that people will love,
-        and then want to experience again and again.
+        I try and bring my design background to my development by trying to think like the people using my products, and working to create great products.
       </P>
       <P>
-        You can check out some of the things I&apos;ve done below, or if you want a quick resume summary, you can grab that right here. Just a warning, this resume isn&apos;t usually as up to date as the rest of this page here, but I do my best to update it when I can.
+        You can check out some of the things I&apos;ve done below, or if you want a quick resume summary, you can grab that right here. Just a warning, this resume isn&apos;t usually as up to date as the rest of this page, but I do my best to update it when I can.
       </P>
       <Resume
         target="_blank"
